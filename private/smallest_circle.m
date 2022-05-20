b59d6b30af36820c1631ce48687e23ba1731c290
Ivo Vellekoop <i.m.vellekoop@utwente.nl>
2022-05-20 21:59:24 +0200

    Fixed bug in smallest_circle, fminsearch not working for complex numbers?

--- private/smallest_circle.m
+++ private/smallest_circle.m
@@ -1,14 +1,13 @@
 function [center, radius] = smallest_circle(points)
 %SMALLEST_CIRCLE Finds the smallest circle that encompasses all points
-%   At the moment, this function only uses a very crude approximation:
-%   just take center as the point exactly between the minimum and maximum 
-%   value in both the x(real) and y(imaginary) direction.
 % (c) Tom Vettenburg
     p = double(points(:));
     center0 = mean(p);
     function r = calc_radius(c)
-        r = max(abs(p - c));
+        cc = c(1) + 1i * c(2);
+        r = max(abs(p - cc));
     end
-    center = fminsearch(@calc_radius, center0);
+    center = fminsearch(@calc_radius, [real(center0), imag(center0)]);
     radius = calc_radius(center);
+    center = center(1) + 1i * center(2);
 end
