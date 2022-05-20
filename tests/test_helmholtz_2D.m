4b4f5e5408b28a993544279c50abeb672e43feba
Ivo Vellekoop <i.m.vellekoop@utwente.nl>
2022-05-20 10:14:51 +0200

    Changed back structure to Nature logo, now with correct refractive index. Found clear superiority for the bicg method!?

--- tests/test_helmholtz_2D.m
+++ tests/test_helmholtz_2D.m
@@ -2,38 +2,22 @@
 %   (c) 2021. Ivo Vellekoop
 %
 
-%% Set up simulation options
-opt = HelmholtzSimOptions(); % clear any previous options
+%% Construct refractive index structure
+% load the logo image, we will use the different color channels for
 oversampling = 1;
+im = imresize(single(imread("natlogo.png"))/255, oversampling, "bilinear");
 n_iron = 2.8954 + 2.9179i;  % Fe at 532nm https://refractiveindex.info/?shelf=main&book=Fe&page=Johnson
-%im = imresize(single(imread("natlogo.png"))/255, oversampling, "bilinear");
-%n = 1 + n_iron * im(:,:,3) * 0.2; % iron
-y = sim.grid.coordinates(1);
-x = sim.grid.coordinates(2);
-grid_extent = sim.grid.pixel_size .* sim.grid.N;
-grid_center = [x(1+floor(end/2)), y(1+floor(end/2))];
-im = exp(-0.5 * ((((x - grid_center(1)) - -0.45 * grid_extent(1)) ./ 1.0).^2 + ((y - grid_center(2)) ./ 10).^2));  % The source position
-im = im .* exp(2i * pi * x);
-im = repmat(im, [1, 1, 3]);
-im(:,:,1) = real(im(:,:,1));
-im(:,:,2) = imag(im(:,:,2));
-im(:,:,3) = 0;  % The object
-positions = [0 0; 1 1; -1 -1] * 10 + grid_center;
-radius = 4;
-for pos_idx = 1:size(positions, 1)
-    im(:,:,3) = im(:,:,3) + ((x - positions(pos_idx, 1)).^2 + (y - positions(pos_idx, 2)).^2 < radius^2);
-end
-n = 1 + (n_iron - 1) * im(:,:,3) * 0.2;  % A fraction of that of iron
-boundary_width = 0.1;
-n = n + 0.2i * max(0, max(abs(x - grid_center(1)) / grid_extent(1), abs(y - grid_center(2)) / grid_extent(2)) - (0.5 - boundary_width)) ./ boundary_width;  % Add absorbing boundary
-opt.N = [size(n,1), size(n,2)];   % Nx, Ny, Nz   (constant in z)
-opt.boundaries_width = 0; %periodic (Setting this to > 0 requires nuttallwin function from Signal Processing Toolbox)
-% opt.callback = DisplayCallback();
-opt.termination_condition = TerminationCondition(relative_limit = 1E-3);
+n = (mean(im, 3) > 0.25) * n_iron;
+
+%% Set up simulation options
+opt = HelmholtzSimOptions();
+opt.boundaries_width = 0; %periodic
+opt.callback = DisplayCallback();
+%opt.termination_condition = TerminationCondition(relative_limit = 1E-3);
 opt.forward_operator = true; % for testing and comparison with MATLAB algorithms
-opt.pixel_size = 0.25/oversampling;
+opt.wavelength = 0.532;
+opt.pixel_size = opt.wavelength/(3*oversampling);
 opt.alpha = 0.75;
-opt.crop_to_roi = false; % so that we can compare with the forward operator
 
 %% create the AnySim object
 sim = HelmholtzSim(n, opt);
