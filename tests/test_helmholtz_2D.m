602d0c2d54e11645ecc28d2a5cfada6bb8a084d0
Ivo Vellekoop <i.m.vellekoop@utwente.nl>
2022-05-20 22:20:28 +0200

    Demo case for advantage of new bias. Does depend on sample a lot

--- tests/test_helmholtz_2D.m
+++ tests/test_helmholtz_2D.m
@@ -5,42 +5,27 @@
 %% Construct refractive index structure
 % load the logo image, we will use the different color channels for
 oversampling = 1;
-im = imresize(single(imread("natlogo.png"))/255, 0.25, "bilinear");
+im = imresize(single(imread("natlogo.png"))/255, 0.5, "bilinear");
 n_iron = 2.8954 + 2.9179i;  % Fe at 532nm https://refractiveindex.info/?shelf=main&book=Fe&page=Johnson
 n_gold = 0.54386 + 2.2309i;
-n_material = n_iron;
-
-%n = (im(:,:,3)+(im(:,:, 1)) > 0.25) * (n_material-1) + 1;
+n_material = (n_iron - 1) * 0.2 + 1;
+n = (im(:,:,3)-(im(:,:, 1)) > 0.25) * (n_material-1) + 1;
 
 %% Set up simulation options
 opt = HelmholtzSimOptions();
-opt.boundaries_width = 20; % 0=periodic
+tol = 0.05;
+opt.boundaries_width = 0; % 0=periodic
 %opt.callback = DisplayCallback();%plot_residual = true);
-opt.forward_operator = true; % for testing and comparison with MATLAB algorithms
-opt.termination_condition = TerminationCondition(relative_limit = 1E-5);
-opt.wavelength = 0.532;
+opt.termination_condition = TerminationCondition(relative_limit = tol);
+%opt.forward_operator = true; % for testing and comparison with MATLAB algorithms
+opt.wavelength = 1;%0.532;
 opt.pixel_size = opt.wavelength/(3*oversampling);
-opt.N = [100, 100];
-
-n = zeros(opt.N);
-grid_center = opt.N/2;
-positions = [0 0; 1 1; -1 -1] * 20 + grid_center;
-radius = 10;
-x = 1:opt.N(1);
-y = (1:opt.N(2)).';
-for pos_idx = 1:size(positions, 1)
-    n = n + ((x - positions(pos_idx, 1)).^2 + (y - positions(pos_idx, 2)).^2 < radius^2);
-end
-n = 1 + (n_iron - 1) * n;
-n_material = n_gold;
-n = (im(:,:,3)-(im(:,:, 1)) > 0.25) * (n_material-1) + 1;
 
 %% create the AnySim object
 sim = HelmholtzSim(n, opt);
 
 %% Define source and run the simulation
-src = exp(-((-3:3).^2 + (-3:3)'.^2)); 
-source = sim.define_source(src, grid_center-3);
+source = sim.define_source(im(:,:,2));
 [E, state] = sim.exec(source);
 
 %%
@@ -65,11 +50,7 @@ simulations = default_simulations("nonsymmetric");
 l_opt = opt;
 l_opt.legacy_mode = true;
 l_sim = HelmholtzSim(n, l_opt);
-l_source = l_sim.define_source(src, grid_center-3);
+l_source = l_sim.define_source(im(:,:,2));
 [l_precond, l_table] = compare_simulations(l_sim, l_source, simulations, tol = tol);
 table(end+1) = "legacy " + l_table(2);
 
-%%
-for i=1:length(precond)
-    fprintf("%f %f\n", norm(precond(i).value(:) - E(:)), norm(l_precond(i).value(:) - E(:)));
-end
